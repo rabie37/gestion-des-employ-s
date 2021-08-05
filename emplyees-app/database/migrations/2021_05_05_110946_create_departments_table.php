9951684c96a6908d648cfe6374d83f6c1bc775bb
EL ADAB <eladabrabie@gmail.com>
2021-08-05 13:35:55 +0100

    site gestion des employees

--- emplyees-app/database/migrations/2021_05_05_110946_create_departments_table.php
+++ emplyees-app/database/migrations/2021_05_05_110946_create_departments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStatesTable extends Migration
+class CreateDepartmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreateStatesTable extends Migration
      */
     public function up()
     {
-        Schema::create('states', function (Blueprint $table) {
+        Schema::create('departments', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ class CreateStatesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('states');
+        Schema::dropIfExists('departments');
     }
 }
